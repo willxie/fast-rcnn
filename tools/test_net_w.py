2531496f60b7563d59f958965fa245cbdadd8ef2
William Xie <wxie@cs.utexas.edu>
2016-02-18 00:12:54 -0600

    Added my versions of the python scripts.

---
+++ tools/test_net_w.py
@@ -0,0 +1,268 @@
+#!/usr/bin/env python
+
+# --------------------------------------------------------
+# Fast R-CNN
+# Copyright (c) 2015 Microsoft
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Ross Girshick
+# --------------------------------------------------------
+
+"""Test a Fast R-CNN network on an image database."""
+
+
+
+import _init_paths
+
+
+from fast_rcnn.config import cfg, get_output_dir
+import argparse
+from utils.timer import Timer
+import numpy as np
+import cv2
+import caffe
+from utils.cython_nms import nms
+import cPickle
+import heapq
+from utils.blob import im_list_to_blob
+import os
+import matplotlib.pyplot as plt
+
+
+from fast_rcnn.test import test_net, im_detect
+# import fast_rcnn.test
+from fast_rcnn.config import cfg, cfg_from_file, cfg_from_list
+from datasets.factory import get_imdb
+import caffe
+import argparse
+import pprint
+import time, os, sys
+
+def parse_args():
+    """
+    Parse input arguments
+    """
+    parser = argparse.ArgumentParser(description='Test a Fast R-CNN network')
+    parser.add_argument('--gpu', dest='gpu_id', help='GPU id to use',
+                        default=0, type=int)
+    parser.add_argument('--def', dest='prototxt',
+                        help='prototxt file defining the network',
+                        default=None, type=str)
+    parser.add_argument('--net', dest='caffemodel',
+                        help='model to test',
+                        default=None, type=str)
+    parser.add_argument('--cfg', dest='cfg_file',
+                        help='optional config file', default=None, type=str)
+    parser.add_argument('--wait', dest='wait',
+                        help='wait until net file exists',
+                        default=True, type=bool)
+    parser.add_argument('--imdb', dest='imdb_name',
+                        help='dataset to test',
+                        default='voc_2007_test', type=str)
+    parser.add_argument('--comp', dest='comp_mode', help='competition mode',
+                        action='store_true')
+    parser.add_argument('--set', dest='set_cfgs',
+                        help='set config keys', default=None,
+                        nargs=argparse.REMAINDER)
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+
+    args = parser.parse_args()
+    return args
+
+
+
+def vis_detections(im, class_name, dets, ax, thresh=0.5):
+    """Draw detected bounding boxes."""
+    inds = np.where(dets[:, -1] >= thresh)[0]
+    # if len(inds) == 0:
+    #     return
+
+    im = im[:, :, (2, 1, 0)]
+
+    ax.imshow(im, aspect='equal')
+    for i in inds:
+        bbox = dets[i, :4]
+        score = dets[i, -1]
+
+        ax.add_patch(
+            plt.Rectangle((bbox[0], bbox[1]),
+                          bbox[2] - bbox[0],
+                          bbox[3] - bbox[1], fill=False,
+                          edgecolor='red', linewidth=3.5)
+            )
+        ax.text(bbox[0], bbox[1] - 2,
+                '{:s} {:.3f}'.format(class_name, score),
+                bbox=dict(facecolor='blue', alpha=0.5),
+                fontsize=14, color='white')
+
+    # ax.set_title(('{} detections with '
+    #               'p({} | box) >= {:.1f}').format(class_name, class_name,
+    #                                               thresh),
+    #               fontsize=14)
+    plt.axis('off')
+    plt.tight_layout()
+    plt.draw()
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    print('Called with args:')
+    print(args)
+
+    if args.cfg_file is not None:
+        cfg_from_file(args.cfg_file)
+    if args.set_cfgs is not None:
+        cfg_from_list(args.set_cfgs)
+
+    print('Using config:')
+    pprint.pprint(cfg)
+
+    while not os.path.exists(args.caffemodel) and args.wait:
+        print('Waiting for {} to exist...'.format(args.caffemodel))
+        time.sleep(10)
+
+    # caffe.set_mode_gpu()
+    # caffe.set_device(args.gpu_id)
+    caffe.set_mode_cpu()
+
+    net = caffe.Net(args.prototxt, args.caffemodel, caffe.TEST)
+    net.name = os.path.splitext(os.path.basename(args.caffemodel))[0]
+
+    imdb = get_imdb(args.imdb_name)
+    imdb.competition_mode(args.comp_mode)
+
+
+#     test_net(net, imdb)
+# def test_net(net, imdb):
+    """Test a Fast R-CNN network on an image database."""
+    num_images = len(imdb.image_index)
+    # heuristic: keep an average of 40 detections per class per images prior
+    # to NMS
+    max_per_set = 40 * num_images
+    # heuristic: keep at most 100 detection per class per image prior to NMS
+    max_per_image = 100
+    # detection thresold for each class (this is adaptively set based on the
+    # max_per_set constraint)
+    thresh = -np.inf * np.ones(imdb.num_classes)
+    # top_scores will hold one minheap of scores per class (used to enforce
+    # the max_per_set constraint)
+    top_scores = [[] for _ in xrange(imdb.num_classes)]
+    # all detections are collected into:
+    #    all_boxes[cls][image] = N x 5 array of detections in
+    #    (x1, y1, x2, y2, score)
+    all_boxes = [[[] for _ in xrange(num_images)]
+                 for _ in xrange(imdb.num_classes)]
+
+    output_dir = get_output_dir(imdb, net)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # timers
+    _t = {'im_detect' : Timer(), 'misc' : Timer()}
+
+    roidb = imdb.roidb
+    for i in xrange(num_images):
+        if i < 215:
+            continue
+        print(imdb.image_path_at(i))
+        im = cv2.imread(imdb.image_path_at(i))
+        _t['im_detect'].tic()
+        scores, boxes = im_detect(net, im, roidb[i]['boxes'])
+        _t['im_detect'].toc()
+
+        CLASSES = ('__background__',
+                   'aeroplane', 'bicycle', 'bird', 'boat',
+                   'bottle', 'bus', 'car', 'cat', 'chair',
+                   'cow', 'diningtable', 'dog', 'horse',
+                   'motorbike', 'person', 'pottedplant',
+                   'sheep', 'sofa', 'train', 'tvmonitor')
+
+        classes = (
+                   'aeroplane', 'bicycle', 'bird', 'boat',
+                   'bottle', 'bus', 'car', 'cat', 'chair',
+                   'cow', 'diningtable', 'dog', 'horse',
+                   'motorbike', 'person', 'pottedplant',
+                   'sheep', 'sofa', 'train', 'tvmonitor')
+
+
+        fig, ax = plt.subplots(figsize=(12, 12))
+
+        # Visualize detections for each class
+        CONF_THRESH = 0.8
+        NMS_THRESH = 0.3
+
+        # print(boxes.shape)   # num_boxes, coordinate of each boxes
+        # print(boxes[0])
+        # raw_input("Press Enter to continue...")
+
+        for cls in classes:
+            cls_ind = CLASSES.index(cls)
+            cls_boxes = boxes[:, 4*cls_ind:4*(cls_ind + 1)]
+
+            cls_scores = scores[:, cls_ind]
+
+            keep = np.where(cls_scores >= CONF_THRESH)[0]
+            cls_boxes = cls_boxes[keep, :]
+
+            cls_scores = cls_scores[keep]
+
+            dets = np.hstack((cls_boxes,
+                              cls_scores[:, np.newaxis])).astype(np.float32)
+            keep = nms(dets, NMS_THRESH)
+            dets = dets[keep, :]
+            # print 'All {} detections with p({} | box) >= {:.1f}'.format(cls, cls,
+            #                                                             CONF_THRESH)
+            vis_detections(im, cls, dets, ax, thresh=CONF_THRESH)
+
+        # plt.show()
+        fig.savefig("/home/users/wxie/fast-rcnn/output/img_box/{:06d}.jpg".format(i))
+        plt.close('all')
+
+        _t['misc'].tic()
+        for j in xrange(1, imdb.num_classes):
+            inds = np.where((scores[:, j] > thresh[j]) &
+                            (roidb[i]['gt_classes'] == 0))[0]
+            cls_scores = scores[inds, j]
+            cls_boxes = boxes[inds, j*4:(j+1)*4]
+            top_inds = np.argsort(-cls_scores)[:max_per_image]
+            cls_scores = cls_scores[top_inds]
+            cls_boxes = cls_boxes[top_inds, :]
+            # push new scores onto the minheap
+            for val in cls_scores:
+                heapq.heappush(top_scores[j], val)
+            # if we've collected more than the max number of detection,
+            # then pop items off the minheap and update the class threshold
+            if len(top_scores[j]) > max_per_set:
+                while len(top_scores[j]) > max_per_set:
+                    heapq.heappop(top_scores[j])
+                thresh[j] = top_scores[j][0]
+
+            all_boxes[j][i] = \
+                    np.hstack((cls_boxes, cls_scores[:, np.newaxis])) \
+                    .astype(np.float32, copy=False)
+
+            if 0:
+                keep = nms(all_boxes[j][i], 0.3)
+                vis_detections(im, imdb.classes[j], all_boxes[j][i][keep, :])
+        _t['misc'].toc()
+
+        print 'im_detect: {:d}/{:d} {:.3f}s {:.3f}s' \
+              .format(i + 1, num_images, _t['im_detect'].average_time,
+                      _t['misc'].average_time)
+
+    for j in xrange(1, imdb.num_classes):
+        for i in xrange(num_images):
+            inds = np.where(all_boxes[j][i][:, -1] > thresh[j])[0]
+            all_boxes[j][i] = all_boxes[j][i][inds, :]
+
+    det_file = os.path.join(output_dir, 'detections.pkl')
+    with open(det_file, 'wb') as f:
+        cPickle.dump(all_boxes, f, cPickle.HIGHEST_PROTOCOL)
+
+    print 'Applying NMS to all detections'
+    nms_dets = apply_nms(all_boxes, cfg.TEST.NMS)
+
+    print 'Evaluating detections'
+    imdb.evaluate_detections(nms_dets, output_dir)
